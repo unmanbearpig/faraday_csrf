9039d1eba6b77681a8dec4b713f24f504b93aac2
unmanbearpig <unmanbearpig@gmail.com>
2015-07-01 01:29:02 +0300

    Error on missing Token value, got rid of Struct

--- lib/faraday_csrf/token.rb
+++ lib/faraday_csrf/token.rb
@@ -2,11 +2,27 @@ require 'faraday'
 
 module Faraday
   class CSRF
-    class Token < Struct.new(:name, :value)
+    class Token
+      class NotFound < RuntimeError; end
+
       attr_reader :name, :value
-      def initialize(name: 'authenticity_token', value:)
-        @name = name
+
+      def initialize(name: nil, value:)
+        unless value
+          raise ArgumentError.new 'Token must have a value'
+        end
+
+        @name = name || self.class.default_name
         @value = value
+      end
+
+      def self.default_name
+        'authenticity_token'
+      end
+
+      def ==(other)
+        name == other.name &&
+          value == other.value
       end
 
       alias_method :to_s, :value
