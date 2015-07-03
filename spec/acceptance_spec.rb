79733b2146c8f8d44f673e1dc3cd5335751feba4
unmanbearpig <unmanbearpig@gmail.com>
2015-07-03 03:43:33 +0300

    Acceptance test

---
+++ spec/acceptance_spec.rb
@@ -0,0 +1,33 @@
+require 'faraday'
+require 'faraday_csrf'
+require 'faraday-cookie_jar'
+require 'vcr_helper'
+
+require 'pry'
+
+describe Faraday::CSRF do
+  let(:url) { 'https://post-ping.herokuapp.com/' }
+
+  let(:connection) do
+    Faraday.new url: url do |conn|
+      conn.use Faraday::CSRF
+
+      conn.request :url_encoded
+      conn.use :cookie_jar
+      conn.adapter Faraday.default_adapter
+    end
+  end
+
+  context 'simple Rails app' do
+    it 'successfully posts to rails app' do
+      post_response = VCR.use_cassette 'simple_rails_app_simple_post' do
+        connection.get '/'
+
+        # POST in rails have csrf protection
+        connection.post '/echo', 'some data': 'blah'
+      end
+
+      expect(post_response.status).to eq 200
+    end
+  end
+end
