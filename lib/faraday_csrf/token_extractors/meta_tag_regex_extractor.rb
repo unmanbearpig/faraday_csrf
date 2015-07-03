3e841a2064510da6720b1a96e6aa2b34eb48f21b
unmanbearpig <unmanbearpig@gmail.com>
2015-07-03 03:44:02 +0300

    Regex extractor

---
+++ lib/faraday_csrf/token_extractors/meta_tag_regex_extractor.rb
@@ -0,0 +1,55 @@
+require 'faraday_csrf/token'
+
+module Faraday
+  class CSRF
+    class MetaTagRegexExtractor
+      attr_reader :html
+
+      class ExtractError < RuntimeError; end
+
+      def initialize(html)
+        @html = html
+      end
+
+      def value
+        content_from_meta_tag('csrf-token')
+      end
+
+      def name
+        content_from_meta_tag('csrf-param')
+      rescue ExtractError
+        nil
+      end
+
+      def token
+        Token.new value: value, name: name
+      rescue RuntimeError
+        raise Token::NotFound.new 'Could not find CSRF token'
+      end
+
+      private
+
+      def content_from_meta_tag tag_name
+        content_from_tag(find_meta_tag(tag_name))
+      end
+
+      def find_meta_tag(name)
+        html.match(/<meta [^>]*name="#{name}"[^>]+>/)[0]
+      rescue NoMethodError
+        raise ExtractError.new "Could not find meta tag with name '#{name}'"
+      end
+
+      def content_from_tag token_string
+        token_string.match(/ content="([^"]+)"/)[1]
+      rescue NoMethodError
+        raise ExtractError.new "Could not find content inside the html tag '#{token_string}'"
+      end
+
+      class << self
+        def extract_from html
+          new(html).token
+        end
+      end
+    end
+  end
+end
