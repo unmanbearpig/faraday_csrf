020409d5b280989cb26a924aeb6629fb43e9092d
unmanbearpig <unmanbearpig@gmail.com>
2015-07-04 10:51:57 +0300

    Ability to expire tokens with #expire!

--- lib/faraday_csrf/token.rb
+++ lib/faraday_csrf/token.rb
@@ -6,6 +6,7 @@ module Faraday
       class NotFound < RuntimeError; end
 
       attr_reader :name, :value
+      protected :name, :value
 
       def initialize(name: nil, value:)
         unless value
@@ -14,24 +15,39 @@ module Faraday
 
         @name = name || self.class.default_name
         @value = value
+        @expired = false
       end
 
       def self.default_name
         'authenticity_token'
       end
 
+      def expire!
+        @name = nil
+        @value = nil
+        @expired = true
+        self
+      end
+
+      def expired?
+        @expired
+      end
+
       def ==(other)
         name == other.name &&
-          value == other.value
+          value == other.value &&
+          expired? == other.expired?
       end
 
       alias_method :to_s, :value
 
       def inspect
+        return "CSRF::Token(~expired~)" if expired?
         "CSRF::Token('#{name}' => '#{value}')"
       end
 
       def to_h
+        return {} if expired?
         { name => value }
       end
     end
--- spec/faraday_csrf/token_spec.rb
+++ spec/faraday_csrf/token_spec.rb
@@ -4,13 +4,8 @@ describe Faraday::CSRF::Token do
   Token = Faraday::CSRF::Token
 
   it 'has a default name' do
-    expect(Token.new(value: 'blah').name)
-      .to eq('authenticity_token')
-  end
-
-  it 'returns its value via to_s method (or to_str?)' do
-    expect(Token.new(value: 'token').to_s)
-      .to eq('token')
+    expect(Token.new(value: 'blah').to_h)
+      .to eq('authenticity_token' => 'blah')
   end
 
   it 'is convertable to hash' do
@@ -22,5 +17,23 @@ describe Faraday::CSRF::Token do
     token = Token.new(name: 'hello', value: 'kitty')
     expect(token.inspect)
       .to eq "CSRF::Token('hello' => 'kitty')"
+  end
+
+  describe '#expire' do
+    subject { Token.new(name: 'hello', value: 'kitty').expire! }
+
+    it 'clears the to_h' do
+      expect(subject.to_h).to eq({})
+    end
+
+    it 'shows that it is expired in inspect' do
+      expect(subject.inspect).to eq 'CSRF::Token(~expired~)'
+    end
+
+    it 'returns itself' do
+      subject = Token.new name: 'hello', value: 'kitty'
+
+      expect(subject.expire!).to eq subject
+    end
   end
 end
